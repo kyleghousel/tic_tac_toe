f468590bf396af50d33204c680989f6fa541a788
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2023-02-02 21:40:29 -0500

    Pseudocode
    base pseudocode

---
+++ READme.md
@@ -0,0 +1,18 @@
+Assignment
+Build a tic-tac-toe game on the command line where two human players can play against each other and the board is displayed in between turns.
+
+Think about how you would set up the different elements within the game… What should be a class? Instance variable? Method? A few minutes of thought can save you from wasting an hour of coding.
+Build your game, taking care to not share information between classes any more than you have to.
+
+
+      |      |
+      |      |
+      |      |
+------+------+------
+      |      |
+      |      |
+      |      |      
+------+------+------
+      |      |
+      |      |
+      |      |
---
+++ tic_tac_toe.rb
@@ -0,0 +1,26 @@
+# 1.Board has 9 empty spaces
+# 2.Two token types
+# 3.Two players
+# 4.One turn at a time, one token placed in one square
+# 5.Player 1 turn even number turn count, Player 2 turn odd number turn count
+# 6.Winning combos top row, mid row, bottom row, left col, mid col, right col, top left to bottom right diag, bottom left to top right diag
+# 7.Tie if board filled with non-whitespace chars and no winning combo
+# 8.New game option
+
+
+#Module to house variables of game that apply to all classes and instances
+module TicTacToe
+
+  #Generation of game tally, and "board"
+  class Game
+
+  end
+
+end
+
+
+#Player class, might need player1 and 2 subclass or may just be instances of Player
+class Player
+
+end
+
