7cb4666ca3df7c595c25fa4f1cd2cbdef1b89331
kyleghousel <69773129+kyleghousel@users.noreply.github.com>
2023-02-08 09:07:32 -0500

    update

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -9,18 +9,104 @@
 
 
 #Module to house variables of game that apply to all classes and instances
-module TicTacToe
+#module TicTacToe
+  #WINNING_COMBOS = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6] ]
 
   #Generation of game tally, and "board"
-  class Game
 
+#end
+
+puts "    
+    XOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXO
+    XOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXO    
+    XOXOXOXO        XOX      OXOX      OXOXOXOXOXO    
+    XOXOXOXOXOX  XOXOXOXO  XOXOXO  XOXOXOXOXOXOXOX    
+    XOXOXOXOXOX  XOXOXOXO  XOXOXO  XOXOXOXOXOXOXOX    
+    XOXOXOXOXOX  XOXOXOXO  XOXOXO  XOXOXOXOXOXOXOX    
+    XOXOXOXOXOX  XOXOXO      XOXO      XOXOXOXOXOX    
+    XOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXO    
+    XOXOXOXO        XOXOX   OXOXO      XOXOXOXOXOX    
+    XOXOXOXOXOX  XOXOXOX  O  XOXO  OXOXOXOXOXOXOXO    
+    XOXOXOXOXOX  XOXOXO       XOX  OXOXOXOXOXOXOXO   
+    XOXOXOXOXOX  XOXOXO   X   XOX  OXOXOXOXOXOXOXO    
+    XOXOXOXOXOX  XOXOX   OXO   XO      XOXOXOXOXOX    
+    XOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXO    
+    XOXOXOXO        XO       XOXO      XOXOXOXOXOX    
+    XOXOXOXOXOX  XOXOX  OXO  XOXO  XOXOXOXOXOXOXOX    
+    XOXOXOXOXOX  XOXOX  OXO  XOXO     XOXOXOXOXOXO    
+    XOXOXOXOXOX  XOXOX  OXO  XOXO  XOXOXOXOXOXOXOX    
+    XOXOXOXOXOX  XOXOX       OXOX      OXOXOXOXOXO    
+    XOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXO
+    XOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXOXO    
+    \n"
+module GameBoard
+  @@board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']    
+end
+
+class Player
+  include GameBoard
+  attr_reader :player, :token
+  @@player_number
+  def initialize(player, token)
+    @player = player
+    @token = token
+  end
+end
+
+class TicTacToe
+  include GameBoard
+  attr_accessor
+  @@game_counter = 0
+  def initialize(player_one, player_two)
+    puts "Time to play! >:)"
+    @@game_counter += 1
+  end
+  
+  def start_game(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  end
+end
+
+
+class TheGame
+  def play_a_round()
+    
+    puts "Player 1, please enter your name:\n"
+    player_one = Player.new(gets.chomp, "X")
+    puts "Welcome, #{player_one.player}, you are playing as X.\n"
+    puts "Player 2, please enter your name:\n"
+    player_two = Player.new(gets.chomp, "O")
+    puts "Welcome, #{player_two.player}, you are playing as O.\n"
+    
+    play_game = TicTacToe.new(player_one, player_two)
+    board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']   
+    play_game.start_game(board)
+    turn_taker()
   end
 
+  def turn_taker
+    turn_counter = 0
+    until turn_counter > 9 do
+      if turn_counter % 2 = 0
+        play = player_one.token
+        puts play
+        turn_counter += 1
+        puts "Your turn Player 1.\n"
+      elsif turn_counter % 2 != 0
+        play = player_two.token
+        put play
+        turn_counter += 1
+        puts "Your turn Player 2.\n"
+      end
+    end
+  end
 end
 
 
-#Player class, might need player1 and 2 subclass or may just be instances of Player
-class Player
 
-end
 
+play_it = TheGame.new()
